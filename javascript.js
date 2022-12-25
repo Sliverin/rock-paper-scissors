56fc6b3ce45e085345afb1c3ddda4cb4a882f687
Sliverin <oldscrolls6848@gmail.com>
2022-12-24 18:47:01 -0800

    fixed

--- javascript.js
+++ javascript.js
@@ -1,21 +1,21 @@
-function getComputerChoice() {
+function computerPlay() {
     let choices = ['rock', 'paper', 'scissors'];
-    return choices[Math.floor(Math.random() * choices.length)]
+    return choices[Math.floor(Math.random() * choices.length)];
 }
 
 function playRound(playerSelection, computerSelection) {
-    const capitalize = word => {return word.charAt(0).toUpperCase() + word.slice(1);}
-    const winStatement = (winner, loser) => {return 'You win! ${capitalize(winner)} beats ${capitalize(loser)}';}
-    const loseStatement = (winner, loser) => {return 'You lose! ${winner)} beats ${capitalize(loser)}';}
-    const winningChoice = {'rock': 'paper', 'paper': 'scissors', 'scissors': 'rock' };
-
+    const capitalize = word => { return word.charAt(0).toUpperCase() + word.slice(1); }
+    const winStatement = (winner, loser) => { return `You Win! ${capitalize(winner)} beats ${capitalize(loser)}`; }
+    const loseStatement = (winner, loser) => { return `You Lose! ${capitalize(winner)} beats ${capitalize(loser)}`; }
+    const winningChoice = {'rock': 'paper', 'paper': 'scissors', 'scissors': 'rock'};
+    
     playerSelection = playerSelection.toLowerCase();
-
-    if(playerSelection === computerSelection) return "Oh! its a tie";
+    
+    if(playerSelection === computerSelection) return "Oh! It's a tie";
     else if(playerSelection === winningChoice[computerSelection]) return winStatement(playerSelection, computerSelection);
     else return loseStatement(computerSelection, playerSelection);
-
 }
+
 const playerSelection = 'rock';
-const computerSelection = getComputerChoice();
+const computerSelection = computerPlay();
 console.log(playRound(playerSelection, computerSelection));
